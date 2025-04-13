4b337d8bd655c2de80ef27ee3640ca4bbddd51f0
33Frok1e <mratikrishna@gmail.com>
2025-04-13 13:08:12 +0530

    types and model done

---
+++ src/app/modules/products/product.model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose"
+import { TInventory, TProduct, TVariants } from "./product.interface"
+
+const VariantSchema = new Schema<TVariants>({
+    type: String,
+    value: String
+})
+
+const InventorySchema = new Schema<TInventory>({
+    quantity: Number,
+    inStock: Boolean
+})
+
+const ProductSchema = new Schema<TProduct>({
+    name: {
+        type: String,
+        require: true
+    },
+    description: String,
+    price: Number,
+    category: String,
+    tags: [String],
+    variants: [VariantSchema],
+    inventory: InventorySchema,
+})
+
+const Product = model('Product', ProductSchema)
